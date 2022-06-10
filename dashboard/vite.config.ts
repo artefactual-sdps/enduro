5ed25317dadc916d23e45165d7ceeefadb7e4d59
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2022-06-10 14:52:31 +0000

    Sort TS/JS imports in the dashboard

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import Pages from "vite-plugin-pages";
 import * as path from "path";
+import { defineConfig } from "vite";
+import Pages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
