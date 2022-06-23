8fe0e91845a7f77043749673189601df6178864e
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2022-06-23 18:13:33 -0400

    Add location column to storage_package table

--- internal/db/migrations/1655396739_storage.up.sql
+++ internal/db/migrations/1655396739_storage.up.sql
@@ -4,7 +4,9 @@ CREATE TABLE storage_package (
   `aip_id` VARCHAR(36) NOT NULL,
   `status` TINYINT NOT NULL, -- {review, rejected, permanent}
   `object_key` VARCHAR(36) NOT NULL,
+  `location` VARCHAR(2048) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `package_aip_id_idx` (`aip_id`),
-  KEY `package_object_key_idx` (`object_key`)
+  KEY `package_object_key_idx` (`object_key`),
+  KEY `package_location_idx` (`location`(50))
 );
