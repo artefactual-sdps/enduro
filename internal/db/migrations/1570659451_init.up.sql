21dd2197a91160f82dc5b8ee538f78d94c623d20
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2022-06-28 19:16:51 -0400

    Update tables
    Add location column to package table and completed_at column to
    preservation_action table.

--- internal/db/migrations/1570659451_init.up.sql
+++ internal/db/migrations/1570659451_init.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE package (
   `workflow_id` VARCHAR(255) NOT NULL,
   `run_id` VARCHAR(36) NOT NULL,
   `aip_id` VARCHAR(36) NOT NULL,
+  `location` VARCHAR(2048) NOT NULL,
   `status` TINYINT NOT NULL, -- {new, in progress, done, error, unknown}
   `created_at` TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL,
   `started_at` TIMESTAMP(6) NULL,
@@ -11,6 +12,7 @@ CREATE TABLE package (
   PRIMARY KEY (`id`),
   KEY `package_name_idx` (`name`(50)),
   KEY `package_aip_id_idx` (`aip_id`),
+  KEY `package_location_idx` (`location`(50)),
   KEY `package_status_idx` (`status`),
   KEY `package_created_at_idx` (`created_at`),
   KEY `package_started_at_idx` (`started_at`)
@@ -21,6 +23,7 @@ CREATE TABLE preservation_action (
   `name` VARCHAR(2048) NOT NULL,
   `status` TINYINT NOT NULL, -- {unspecified, complete, processing, failed}
   `started_at` TIMESTAMP(6) NULL,
+  `completed_at` TIMESTAMP(6) NULL,
   `package_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`package_id`) REFERENCES package(`id`)
--- internal/db/migrations/1655396739_storage.up.sql
+++ internal/db/migrations/1655396739_storage.up.sql
@@ -2,11 +2,11 @@ CREATE TABLE storage_package (
   `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
   `name` VARCHAR(2048) NOT NULL,
   `aip_id` VARCHAR(36) NOT NULL,
+  `location` VARCHAR(2048) NOT NULL,
   `status` TINYINT NOT NULL, -- {in_review, rejected, stored}
   `object_key` VARCHAR(36) NOT NULL,
-  `location` VARCHAR(2048) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `package_aip_id_idx` (`aip_id`),
-  KEY `package_object_key_idx` (`object_key`),
-  KEY `package_location_idx` (`location`(50))
+  KEY `package_location_idx` (`location`(50)),
+  KEY `package_object_key_idx` (`object_key`)
 );
