a0765fe2efb11aef9a185fc8b3a05c004936a308
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2022-07-01 11:59:48 -0400

    Add tests for storage service workflows

---
+++ internal/storage/workflows/move_test.go
@@ -0,0 +1,47 @@
+package workflows
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	temporalsdk_activity "go.temporal.io/sdk/activity"
+	temporalsdk_testsuite "go.temporal.io/sdk/testsuite"
+
+	"github.com/artefactual-labs/enduro/internal/storage"
+	"github.com/artefactual-labs/enduro/internal/storage/activities"
+	"github.com/artefactual-labs/enduro/internal/storage/fake"
+)
+
+func TestStorageMoveWorkflow(t *testing.T) {
+	s := temporalsdk_testsuite.WorkflowTestSuite{}
+	env := s.NewTestWorkflowEnvironment()
+
+	aipID := uuid.NewString()
+	location := "perma-aips-100"
+
+	// Mock services and their expected calls
+	ctrl := gomock.NewController(t)
+	storagesvc := fake.NewMockService(ctrl)
+	storagesvc.EXPECT().Delete(gomock.Any(), aipID)
+	storagesvc.EXPECT().UpdatePackageLocation(gomock.Any(), location, aipID)
+	storagesvc.EXPECT().UpdatePackageStatus(gomock.Any(), storage.StatusStored, aipID)
+
+	// Worker activities
+	env.RegisterActivityWithOptions(activities.NewCopyToPermanentLocationActivity(storagesvc).Execute, temporalsdk_activity.RegisterOptions{Name: storage.CopyToPermanentLocationActivityName})
+	env.OnActivity(storage.CopyToPermanentLocationActivityName, mock.Anything, mock.Anything).Return(nil)
+
+	env.ExecuteWorkflow(
+		NewStorageMoveWorkflow(storagesvc).Execute,
+		storage.StorageMoveWorkflowRequest{
+			AIPID:    aipID,
+			Location: location,
+		},
+	)
+
+	require.True(t, env.IsWorkflowCompleted())
+	err := env.GetWorkflowResult(nil)
+	require.NoError(t, err)
+}
---
+++ internal/storage/workflows/upload_test.go
@@ -0,0 +1,38 @@
+package workflows
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	temporalsdk_testsuite "go.temporal.io/sdk/testsuite"
+
+	"github.com/artefactual-labs/enduro/internal/storage"
+)
+
+func TestStorageUploadWorkflow(t *testing.T) {
+	s := temporalsdk_testsuite.WorkflowTestSuite{}
+	env := s.NewTestWorkflowEnvironment()
+
+	// Signal handler
+	env.RegisterDelayedCallback(
+		func() {
+			env.SignalWorkflow(
+				storage.UploadDoneSignalName,
+				storage.UploadDoneSignal{},
+			)
+		},
+		0,
+	)
+
+	env.ExecuteWorkflow(
+		NewStorageUploadWorkflow().Execute,
+		storage.StorageUploadWorkflowRequest{
+			AIPID: uuid.NewString(),
+		},
+	)
+
+	require.True(t, env.IsWorkflowCompleted())
+	err := env.GetWorkflowResult(nil)
+	require.NoError(t, err)
+}
