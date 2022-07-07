4111a63b7dbe83bc874c1a6aa11acd8706d10a1e
Jesús García Crespo <jesus@sevein.com>
2022-07-07 19:22:58 +0200

    Update bingo deps

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -17,17 +17,17 @@ GO     ?= $(shell which go)
 #	@echo "Running go-mod-upgrade"
 #	@$(GO_MOD_UPGRADE) <flags/args..>
 #
-GO_MOD_UPGRADE := $(GOBIN)/go-mod-upgrade-v0.7.0
+GO_MOD_UPGRADE := $(GOBIN)/go-mod-upgrade-v0.8.0
 $(GO_MOD_UPGRADE): $(BINGO_DIR)/go-mod-upgrade.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/go-mod-upgrade-v0.7.0"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=go-mod-upgrade.mod -o=$(GOBIN)/go-mod-upgrade-v0.7.0 "github.com/oligot/go-mod-upgrade"
+	@echo "(re)installing $(GOBIN)/go-mod-upgrade-v0.8.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=go-mod-upgrade.mod -o=$(GOBIN)/go-mod-upgrade-v0.8.0 "github.com/oligot/go-mod-upgrade"
 
-GOA := $(GOBIN)/goa-v3.7.7
+GOA := $(GOBIN)/goa-v3.7.10
 $(GOA): $(BINGO_DIR)/goa.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/goa-v3.7.7"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goa.mod -o=$(GOBIN)/goa-v3.7.7 "goa.design/goa/v3/cmd/goa"
+	@echo "(re)installing $(GOBIN)/goa-v3.7.10"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goa.mod -o=$(GOBIN)/goa-v3.7.10 "goa.design/goa/v3/cmd/goa"
 
 GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.46.2
 $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
@@ -35,17 +35,17 @@ $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
 	@echo "(re)installing $(GOBIN)/golangci-lint-v1.46.2"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.46.2 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
-GORELEASER := $(GOBIN)/goreleaser-v1.4.1
+GORELEASER := $(GOBIN)/goreleaser-v1.10.1
 $(GORELEASER): $(BINGO_DIR)/goreleaser.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/goreleaser-v1.4.1"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goreleaser.mod -o=$(GOBIN)/goreleaser-v1.4.1 "github.com/goreleaser/goreleaser"
+	@echo "(re)installing $(GOBIN)/goreleaser-v1.10.1"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goreleaser.mod -o=$(GOBIN)/goreleaser-v1.10.1 "github.com/goreleaser/goreleaser"
 
-GOTESTSUM := $(GOBIN)/gotestsum-v1.7.0
+GOTESTSUM := $(GOBIN)/gotestsum-v1.8.1
 $(GOTESTSUM): $(BINGO_DIR)/gotestsum.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/gotestsum-v1.7.0"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=gotestsum.mod -o=$(GOBIN)/gotestsum-v1.7.0 "gotest.tools/gotestsum"
+	@echo "(re)installing $(GOBIN)/gotestsum-v1.8.1"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=gotestsum.mod -o=$(GOBIN)/gotestsum-v1.8.1 "gotest.tools/gotestsum"
 
 MOCKGEN := $(GOBIN)/mockgen-v1.6.0
 $(MOCKGEN): $(BINGO_DIR)/mockgen.mod
