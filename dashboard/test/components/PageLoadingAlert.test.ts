1ca54fdb54d3407e35535a738d2116bdbf7aed60
Jesús García Crespo <jesus@sevein.com>
2022-07-11 19:27:17 +0200

    Add tests for PackageLocationCard

--- dashboard/test/components/PageLoadingAlert.test.ts
+++ dashboard/test/components/PageLoadingAlert.test.ts
@@ -1,12 +1,11 @@
 import PageLoadingAlert from "../../src/components/PageLoadingAlert.vue";
-import { mount, RouterLinkStub } from "@vue/test-utils";
+import { render, fireEvent } from "@testing-library/vue";
+import { RouterLinkStub } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 
 describe("PageLoadingAlert.vue", () => {
   it("should render", () => {
-    expect(PageLoadingAlert).toBeTruthy();
-
-    const wrapper = mount(PageLoadingAlert, {
+    const { html } = render(PageLoadingAlert, {
       props: {
         error: { response: { status: 404 } },
       },
@@ -17,6 +16,15 @@ describe("PageLoadingAlert.vue", () => {
       },
     });
 
-    expect(wrapper.html()).toContain("Page not found!");
+    expect(html()).toMatchInlineSnapshot(`
+      "<!-- Not found. -->
+      <div class=\\"alert alert-warning\\" role=\\"alert\\">
+        <h4 class=\\"alert-heading\\">Page not found!</h4>
+        <p>We can't find the page you're looking for.</p>
+        <hr><a class=\\"btn btn-warning\\">Take me home</a>
+      </div>
+      <!-- Other errors. -->
+      <!--v-if-->"
+    `);
   });
 });
