21c99f4af3148c86f036d0892a1954342f706ec8
Jesús García Crespo <jesus@sevein.com>
2022-07-22 20:00:48 +0200

    Make the header navbar collapsible

--- dashboard/src/components/Header.vue
+++ dashboard/src/components/Header.vue
@@ -1,9 +1,33 @@
+<script setup lang="ts">
+import Collapse from "bootstrap/js/dist/collapse";
+import { onMounted } from "vue";
+
+const el = $ref<HTMLElement | null>(null);
+
+onMounted(() => {
+  if (el) new Collapse(el);
+});
+</script>
+
 <template>
-  <nav class="navbar navbar-dark navbar-expand-lg">
+  <nav class="navbar navbar-dark navbar-expand-sm">
     <div class="container-fluid">
       <router-link class="navbar-brand mb-0 h1" :to="{ name: 'index' }"
         >Enduro</router-link
       >
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        ref="collapseButton"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto">
