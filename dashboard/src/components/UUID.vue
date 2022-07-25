671469e8352bc22f9491befc8744007b341c8e37
José Raddaoui Marín <raddaouimarin@gmail.com>
2022-07-25 19:49:05 +0200

    Remove transition

--- dashboard/src/components/UUID.vue
+++ dashboard/src/components/UUID.vue
@@ -17,50 +17,21 @@ const { copy, copied, isSupported } = useClipboard({ source });
     <span class="font-monospace">{{ id }}</span>
 
     <template v-if="isSupported">
-      <button @click="copy()" class="btn btn-sm btn-link link-secondary ms-2">
-        <Transition name="slide-up" :duration="200">
-          <!-- Copied visual hint. -->
-          <span v-if="copied">
-            <IconCheck aria-hidden="true" class="text-success" />
-            <span class="visually-hidden">Copied!</span>
-          </span>
-          <!-- Copy icon. -->
-          <span v-else>
-            <IconCopy aria-hidden="true" />
-            <span class="visually-hidden">Copy to clipboard</span>
-          </span>
-        </Transition>
+      <button
+        @click="copy()"
+        class="btn btn-sm btn-link link-secondary p-0 ms-2"
+      >
+        <!-- Copied visual hint. -->
+        <span v-if="copied">
+          <IconCheck aria-hidden="true" class="text-success" />
+          <span class="visually-hidden">Copied!</span>
+        </span>
+        <!-- Copy icon. -->
+        <span v-else>
+          <IconCopy aria-hidden="true" />
+          <span class="visually-hidden">Copy to clipboard</span>
+        </span>
       </button>
     </template>
   </div>
 </template>
-
-<style scoped lang="scss">
-button {
-  position: relative;
-  width: $spacer * 1.2;
-  height: $spacer * 1.5;
-  overflow: hidden;
-
-  span {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-}
-
-.slide-up-enter-active,
-.slide-up-leave-active {
-  transition: all 0.2s ease-out;
-}
-
-.slide-up-enter-from {
-  opacity: 0;
-  transform: translateY($spacer);
-}
-
-.slide-up-leave-to {
-  opacity: 0;
-  transform: translateY(-$spacer);
-}
-</style>
