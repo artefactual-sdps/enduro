6d6dc0290b69d2d3a19b652c67ee7664756ce03c
José Raddaoui Marín <raddaouimarin@gmail.com>
2022-07-26 16:00:42 +0200

    Test UUID clipboard copy

--- dashboard/test/components/UUID.test.ts
+++ dashboard/test/components/UUID.test.ts
@@ -1,18 +1,33 @@
 import UUID from "@/components/UUID.vue";
-import { render, cleanup } from "@testing-library/vue";
+import { cleanup, fireEvent, render } from "@testing-library/vue";
 import { afterEach, describe, expect, it } from "vitest";
 
 describe("UUID.vue", () => {
+  const uuid = "31ceb5d5-a9c1-488b-b4ee-40910e54109e";
+
   afterEach(() => cleanup());
 
   it("should render", () => {
-    const { getByText } = render(UUID, {
-      props: {
-        id: "31ceb5d5-a9c1-488b-b4ee-40910e54109e",
-      },
+    const { getByText } = render(UUID, { props: { id: uuid } });
+
+    expect(getByText(uuid).className).toBe("font-monospace");
+  });
+
+  it("should copy to clipboard", async () => {
+    let clipboardText = "";
+    Object.assign(navigator, {
+      clipboard: { writeText: (text: string) => (clipboardText = text) },
     });
 
-    const el = getByText("31ceb5d5-a9c1-488b-b4ee-40910e54109e");
-    expect(el.className).toBe("font-monospace");
+    const { getByRole } = render(UUID, { props: { id: uuid } });
+
+    await fireEvent.click(getByRole("button", { name: "Copy to clipboard" }));
+
+    expect(clipboardText).toEqual(uuid);
+    getByRole("button", { name: "Copied!" });
+
+    setTimeout(() => {
+      getByRole("button", { name: "Copy to clipboard" });
+    }, 500);
   });
 });
