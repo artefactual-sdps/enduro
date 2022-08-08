591dcb5ec7ff773a188b03a41921eca1241a0b53
José Raddaoui Marín <raddaouimarin@gmail.com>
2022-08-08 14:10:24 +0200

    Upgrade bingo

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -1,4 +1,4 @@
-# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.5.2. DO NOT EDIT.
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.6. DO NOT EDIT.
 # All tools are designed to be build inside $GOBIN.
 BINGO_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 GOPATH ?= $(shell go env GOPATH)
@@ -7,33 +7,39 @@ GO     ?= $(shell which go)
 
 # Below generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
-# For example for go-mod-upgrade variable:
+# For example for bingo variable:
 #
 # In your main Makefile (for non array binaries):
 #
 #include .bingo/Variables.mk # Assuming -dir was set to .bingo .
 #
-#command: $(GO_MOD_UPGRADE)
-#	@echo "Running go-mod-upgrade"
-#	@$(GO_MOD_UPGRADE) <flags/args..>
+#command: $(BINGO)
+#	@echo "Running bingo"
+#	@$(BINGO) <flags/args..>
 #
-GO_MOD_UPGRADE := $(GOBIN)/go-mod-upgrade-v0.8.0
+BINGO := $(GOBIN)/bingo-v0.6.0
+$(BINGO): $(BINGO_DIR)/bingo.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/bingo-v0.6.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=bingo.mod -o=$(GOBIN)/bingo-v0.6.0 "github.com/bwplotka/bingo"
+
+GO_MOD_UPGRADE := $(GOBIN)/go-mod-upgrade-v0.9.0
 $(GO_MOD_UPGRADE): $(BINGO_DIR)/go-mod-upgrade.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/go-mod-upgrade-v0.8.0"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=go-mod-upgrade.mod -o=$(GOBIN)/go-mod-upgrade-v0.8.0 "github.com/oligot/go-mod-upgrade"
+	@echo "(re)installing $(GOBIN)/go-mod-upgrade-v0.9.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=go-mod-upgrade.mod -o=$(GOBIN)/go-mod-upgrade-v0.9.0 "github.com/oligot/go-mod-upgrade"
 
-GOA := $(GOBIN)/goa-v3.7.10
+GOA := $(GOBIN)/goa-v3.8.1
 $(GOA): $(BINGO_DIR)/goa.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/goa-v3.7.10"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goa.mod -o=$(GOBIN)/goa-v3.7.10 "goa.design/goa/v3/cmd/goa"
+	@echo "(re)installing $(GOBIN)/goa-v3.8.1"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goa.mod -o=$(GOBIN)/goa-v3.8.1 "goa.design/goa/v3/cmd/goa"
 
-GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.46.2
+GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.48.0
 $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/golangci-lint-v1.46.2"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.46.2 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	@echo "(re)installing $(GOBIN)/golangci-lint-v1.48.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.48.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
 GOTESTSUM := $(GOBIN)/gotestsum-v1.8.1
 $(GOTESTSUM): $(BINGO_DIR)/gotestsum.mod
