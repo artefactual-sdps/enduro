5d6a40b611b38f5e0935ff816223afbac8fd915b
Jesús García Crespo <jesus@sevein.com>
2022-08-31 14:27:12 +0200

    Bump golangci-lint from 1.48.0 to 1.49.0

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -41,11 +41,11 @@ $(GOA): $(BINGO_DIR)/goa.mod
 	@echo "(re)installing $(GOBIN)/goa-v3.8.4"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goa.mod -o=$(GOBIN)/goa-v3.8.4 "goa.design/goa/v3/cmd/goa"
 
-GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.48.0
+GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.49.0
 $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/golangci-lint-v1.48.0"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.48.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	@echo "(re)installing $(GOBIN)/golangci-lint-v1.49.0"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.49.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
 GOTESTSUM := $(GOBIN)/gotestsum-v1.8.1
 $(GOTESTSUM): $(BINGO_DIR)/gotestsum.mod
