269c96da72564091a2166a5d5bb6c7d6d2e5e08c
Jesús García Crespo <jesus@sevein.com>
2022-10-10 20:28:42 +0000

    Bump goa from 3.8.5 to 3.9.1

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -35,11 +35,11 @@ $(GO_MOD_UPGRADE): $(BINGO_DIR)/go-mod-upgrade.mod
 	@echo "(re)installing $(GOBIN)/go-mod-upgrade-v0.9.1"
 	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=go-mod-upgrade.mod -o=$(GOBIN)/go-mod-upgrade-v0.9.1 "github.com/oligot/go-mod-upgrade"
 
-GOA := $(GOBIN)/goa-v3.8.5
+GOA := $(GOBIN)/goa-v3.9.1
 $(GOA): $(BINGO_DIR)/goa.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/goa-v3.8.5"
-	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goa.mod -o=$(GOBIN)/goa-v3.8.5 "goa.design/goa/v3/cmd/goa"
+	@echo "(re)installing $(GOBIN)/goa-v3.9.1"
+	@cd $(BINGO_DIR) && $(GO) build -mod=mod -modfile=goa.mod -o=$(GOBIN)/goa-v3.9.1 "goa.design/goa/v3/cmd/goa"
 
 GOTESTSUM := $(GOBIN)/gotestsum-v1.8.2
 $(GOTESTSUM): $(BINGO_DIR)/gotestsum.mod
