e5d256e064031112b1293a701e96d3044e179b6e
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-09-27 11:46:17 -0400

    Add req as field name to struct

--- internal/workflow/processing_test.go
+++ internal/workflow/processing_test.go
@@ -34,7 +34,7 @@ type ProcessingWorkflowTestSuite struct {
 func TestTransferInfo_Name(t *testing.T) {
 	t.Run("Returns name of transfer", func(t *testing.T) {
 		tinfo := TransferInfo{}
-		tinfo.Key = "somename.tar.gz"
+		tinfo.req.Key = "somename.tar.gz"
 		assert.Equal(t, tinfo.Name(), "somename")
 	})
 }
