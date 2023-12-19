4097fe37ce886c555eecf2a17f0903c4d2840786
José Raddaoui Marín <raddaouimarin@gmail.com>
2023-12-19 18:33:43 +0100

    Establish WebSocket connection after login
    Refs #811.

--- dashboard/src/pages/user/signin-callback.vue
+++ dashboard/src/pages/user/signin-callback.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import auth from "@/auth";
+import { client } from "@/client";
 import { useLayoutStore } from "@/stores/layout";
 import { useRouter } from "vue-router/auto";
 
 const router = useRouter();
 auth.signinCallback().then((user) => {
   useLayoutStore().setUser(user || null);
+  if (user) {
+    client.package.packageMonitorRequest().then(() => {
+      client.connectPackageMonitor();
+    });
+  }
   router.push({ name: "/" });
 });
 </script>
