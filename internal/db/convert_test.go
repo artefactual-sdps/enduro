5e43d200c6780343db6a60d1f991433c084d72b0
David Juhasz <djjuhasz@gmail.com>
2024-03-26 17:40:46 -0700

    Add tests for DB data converters

---
+++ internal/db/convert_test.go
@@ -0,0 +1,41 @@
+package db_test
+
+import (
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/artefactual-sdps/enduro/internal/db"
+	"gotest.tools/v3/assert"
+)
+
+func TestFormatOptionalString(t *testing.T) {
+	t.Run("Returns nil pointer for an empty string", func(t *testing.T) {
+		t.Parallel()
+		got := db.FormatOptionalString("")
+		assert.Assert(t, got == nil)
+	})
+
+	t.Run("Returns a pointer to a string", func(t *testing.T) {
+		t.Parallel()
+		got := db.FormatOptionalString("foo")
+		assert.Equal(t, *got, "foo")
+	})
+}
+
+func TestFormatOptionalTime(t *testing.T) {
+	t.Run("Returns nil pointer for null time", func(t *testing.T) {
+		t.Parallel()
+		got := db.FormatOptionalTime(sql.NullTime{})
+		assert.Assert(t, got == nil)
+	})
+
+	t.Run("Returns an RFC3339 time string", func(t *testing.T) {
+		t.Parallel()
+		got := db.FormatOptionalTime(sql.NullTime{
+			Time:  time.Date(2024, 3, 6, 11, 57, 17, 115, time.UTC),
+			Valid: true,
+		})
+		assert.Equal(t, *got, "2024-03-06T11:57:17Z")
+	})
+}
