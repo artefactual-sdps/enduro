751dbec1e466b76cf9c953e39dd784e99cefd643
David Juhasz <djjuhasz@gmail.com>
2024-04-04 16:50:02 -0700

    Bump workflowcheck to version 0.2.0

--- hack/make/dep_workflowcheck.mk
+++ hack/make/dep_workflowcheck.mk
@@ -3,14 +3,13 @@ $(call _conditional_include,$(MAKEDIR)/base.mk)
 $(call _assert_var,CACHE_VERSIONS)
 $(call _assert_var,CACHE_BIN)
 
-# Tag contrib/tools/workflowcheck/v0.1.0 isn't compatible with go1.22.
-WORKFLOWCHECK_VERSION ?= 72a5b6f
+WORKFLOWCHECK_VERSION ?= 0.2.0
 
 WORKFLOWCHECK := $(CACHE_VERSIONS)/workflowcheck/$(WORKFLOWCHECK_VERSION)
 $(WORKFLOWCHECK):
 	rm -f $(CACHE_BIN)/workflowcheck
 	mkdir -p $(CACHE_BIN)
-	env GOBIN=$(CACHE_BIN) go install go.temporal.io/sdk/contrib/tools/workflowcheck@$(WORKFLOWCHECK_VERSION)
+	env GOBIN=$(CACHE_BIN) go install go.temporal.io/sdk/contrib/tools/workflowcheck@v$(WORKFLOWCHECK_VERSION)
 	chmod +x $(CACHE_BIN)/workflowcheck
 	rm -rf $(dir $(WORKFLOWCHECK))
 	mkdir -p $(dir $(WORKFLOWCHECK))
