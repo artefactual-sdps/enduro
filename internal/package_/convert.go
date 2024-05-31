6e5a324540a057f226c840dc182aa34b4c146dcf
David Juhasz <djjuhasz@gmail.com>
2024-05-31 15:23:34 -0700

    Add persistence CreatePreservationAction method
    - Add a `CreatePreservationAction` method to the persistence service,
      the telemetry wrapper, and the ent client implementation
    - Update `package_.CreatePreservationAction()` to use the persistence
      service method

--- internal/package_/convert.go
+++ internal/package_/convert.go
@@ -1,12 +1,32 @@
 package package_
 
 import (
+	"time"
+
 	"go.artefactual.dev/tools/ref"
 
 	goapackage "github.com/artefactual-sdps/enduro/internal/api/gen/package_"
 	"github.com/artefactual-sdps/enduro/internal/datatypes"
 	"github.com/artefactual-sdps/enduro/internal/db"
 )
+
+// preservationActionToGoa returns the API representation of a preservation task.
+func preservationActionToGoa(pt *datatypes.PreservationAction) *goapackage.EnduroPackagePreservationAction {
+	var startedAt string
+	if pt.StartedAt.Valid {
+		startedAt = pt.StartedAt.Time.Format(time.RFC3339)
+	}
+
+	return &goapackage.EnduroPackagePreservationAction{
+		ID:          pt.ID,
+		WorkflowID:  pt.WorkflowID,
+		Type:        pt.Type.String(),
+		Status:      pt.Status.String(),
+		StartedAt:   startedAt,
+		CompletedAt: db.FormatOptionalTime(pt.CompletedAt),
+		PackageID:   &pt.PackageID,
+	}
+}
 
 // preservationTaskToGoa returns the API representation of a preservation task.
 func preservationTaskToGoa(pt *datatypes.PreservationTask) *goapackage.EnduroPackagePreservationTask {
