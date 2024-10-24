ea923274f37db0ac0ccb34f7f5b7430d9bb4fbab
David Juhasz <djjuhasz@gmail.com>
2024-10-24 12:14:56 -0700

    Use Tabs component for package status filter
    Refs #989.
    
    Adapt the existing Tabs component to filter the package list by status.
    
    Changes:
    - Make the dropdown package status filter a component to allow switching
      between dropdown and tab filters for A/B testing
    - Allow setting the package status filter by a "status" URL query
      parameter
    - Update the "status" query parameter when clicking a filter tab
    - Make the tab icons optional
    - Include query parameters when checking if a tab is active

---
+++ dashboard/src/components/PackageListSelectFilters.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { usePackageStore } from "@/stores/package";
+import { PackageListStatusEnum } from "@/openapi-generator";
+
+const packageStore = usePackageStore();
+</script>
+
+<template>
+  <div class="d-flex flex-wrap gap-3 mb-3 p-3 border bg-light">
+    <h3 class="mb-0 pe-3 border-end">Filters</h3>
+    <div class="d-flex gap-2">
+      <label for="filter-status" class="align-self-center">Status</label>
+      <select
+        id="filter-status"
+        v-model="packageStore.filters.status"
+        @change="packageStore.fetchPackages(1)"
+        class="form-select"
+      >
+        <option value="">any</option>
+        <option v-for="item of PackageListStatusEnum" :value="item">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
