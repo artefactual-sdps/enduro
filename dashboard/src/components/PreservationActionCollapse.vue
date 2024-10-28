cd20bdb922e517911a5efb70ea53b3d0f4a05153
Daniel Cosme <cosmedaniel8@gmail.com>
2024-10-28 14:52:02 -0500

    Change CSS class name

--- dashboard/src/components/PreservationActionCollapse.vue
+++ dashboard/src/components/PreservationActionCollapse.vue
@@ -138,7 +138,7 @@ watch($$(expandCounter), () => show());
             <td>
               <StatusBadge :status="task.status" />
             </td>
-            <td class="line-wrap">{{ task.note }}</td>
+            <td class="line-break">{{ task.note }}</td>
           </tr>
         </tbody>
       </table>
