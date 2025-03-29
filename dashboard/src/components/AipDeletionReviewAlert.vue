21e27682ff751a3c15c2a058f3c048e7dce058d2
José Raddaoui Marín <raddaouimarin@gmail.com>
2025-03-29 19:17:02 +0100

    Remove unnecessary computed

--- dashboard/src/components/AipDeletionReviewAlert.vue
+++ dashboard/src/components/AipDeletionReviewAlert.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { computed } from "vue";
-
 import { useAipStore } from "@/stores/aip";
 
 const aipStore = useAipStore();
@@ -8,8 +6,6 @@ const aipStore = useAipStore();
 const { note } = defineProps<{
   note: string;
 }>();
-
-const body = computed(() => note);
 
 const review = async (approved: boolean) => {
   // TODO: Add confirmation dialog.
@@ -20,7 +16,7 @@ const review = async (approved: boolean) => {
 <template>
   <div class="alert alert-info" role="alert" v-if="aipStore.isPending">
     <h4 class="alert-heading">Task: Review Delete AIP request</h4>
-    <p class="line-break">{{ body }}</p>
+    <p class="line-break">{{ note }}</p>
     <hr />
     <div class="d-flex flex-wrap gap-2">
       <button class="btn btn-success" type="button" @click="review(true)">
