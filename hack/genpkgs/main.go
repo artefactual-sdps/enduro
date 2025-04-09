3657617fa1a465ac8a1abab69fc5c9048fdc1291
David Juhasz <djjuhasz@gmail.com>
2025-04-09 16:59:39 -0700

    Add AIP generation to sample data generator
    - Add a CLI argument to Go package generator script to select AIP or
      SIP generation
    - Update the load.sh script to generate and load sample AIP data as
      well as the sample SIPs

--- hack/genpkgs/main.go
+++ hack/genpkgs/main.go
@@ -16,22 +16,39 @@ const (
 
 	// Size of each batch that we write to the CSV.
 	batchSize = 100
+
+	usage = `Usage: genpkgs TYPE
+
+Generate a dataset of packages for testing.
+
+TYPE: "sip" or "aip"
+`
 )
 
 func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		log.Fatal(usage)
+	}
+	if args[0] != "sip" && args[0] != "aip" {
+		log.Fatal(usage)
+	}
+
 	w := csv.NewWriter(os.Stdout)
 
-	var c int
-	for {
-		c++
-		if err := w.Write(gen(c)); err != nil {
+	for i := range datasetSize {
+		var row []string
+		if args[0] == "sip" {
+			row = sip(i + 1)
+		} else {
+			row = aip(i + 1)
+		}
+
+		if err := w.Write(row); err != nil {
 			log.Println("error writing record to csv:", err)
 		}
-		if c%batchSize == 0 {
+		if i%batchSize == 0 {
 			w.Flush()
-		}
-		if c == datasetSize {
-			break
 		}
 	}
 
@@ -45,15 +62,27 @@ func id() string {
 	return uuid.New().String()
 }
 
-func gen(c int) []string {
+func sip(c int) []string {
 	const doneStatus string = "2"
 	return []string{
-		strconv.Itoa(c),
+		strconv.Itoa(c),                     // id
 		fmt.Sprintf("DPJ-SIP-%s.tar", id()), // name
 		id(),                                // aip_id
 		doneStatus,                          // status
 		"2019-11-21 17:36:10",               // created_at
 		"2019-11-21 17:36:11",               // started_at
 		"2019-11-21 17:42:12",               // completed_at
+	}
+}
+
+func aip(i int) []string {
+	return []string{
+		strconv.Itoa(i),             // id
+		fmt.Sprintf("AIP-%s", id()), // name
+		id(),                        // aip_id
+		"stored",                    // status
+		id(),                        // object_key
+		"1",                         // location_id
+		"2019-11-21 17:43:51",       // created_at
 	}
 }
