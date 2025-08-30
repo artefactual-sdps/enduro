81797200af878eb045babaf08058a958b7e291cd
David Juhasz <djjuhasz@gmail.com>
2025-08-29 17:16:56 -0700

    WIP: Add institutional logo to Dashboard

--- dashboard/src/components/Header.vue
+++ dashboard/src/components/Header.vue
@@ -10,69 +10,99 @@ import IconMenu from "~icons/clarity/menu-line";
 const layoutStore = useLayoutStore();
 
 const showAbout = async () => await openDialog(AboutDialogVue);
+
+const instLogo = import.meta.env.VITE_INSTITUTION_LOGO;
+const instURL = import.meta.env.VITE_INSTITUTION_URL;
+const instName = import.meta.env.VITE_INSTITUTION_NAME;
 </script>
 
 <template>
   <header class="bg-white border-bottom sticky-top">
     <nav class="navbar navbar-expand-md p-0">
-      <!-- Open offcanvas button, only visible in sm. -->
-      <button
-        type="button"
-        class="navbar-toggler btn btn-link text-decoration-none p-3"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#menu-offcanvas"
-        aria-controls="menu-offcanvas"
-        aria-label="Open navigation"
-      >
-        <IconMenu
-          class="text-dark mx-1"
-          style="font-size: 1.5em"
-          aria-hidden="true"
+      <div class="container-fluid">
+        <!-- Open offcanvas button, only visible in sm. -->
+        <button
+          type="button"
+          class="navbar-toggler btn btn-link text-decoration-none p-3"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#menu-offcanvas"
+          aria-controls="menu-offcanvas"
+          aria-label="Open navigation"
+        >
+          <IconMenu
+            class="text-dark mx-1"
+            style="font-size: 1.5em"
+            aria-hidden="true"
+          />
+        </button>
+
+        <!-- Collapse/expand sidebar button, visible in md or higher. -->
+        <button
+          type="button"
+          class="btn btn-link text-decoration-none p-3 d-none d-md-block"
+          :class="layoutStore.sidebarCollapsed ? 'sidebar-collapsed' : ''"
+          :aria-label="
+            (layoutStore.sidebarCollapsed ? 'Expand' : 'Collapse') +
+            ' navigation'
+          "
+          @click="layoutStore.toggleSidebar()"
+        >
+          <IconMenu
+            class="text-dark mx-1"
+            style="font-size: 1.5em"
+            aria-hidden="true"
+          />
+        </button>
+
+        <router-link
+          class="navbar-brand h1 mb-0 me-auto p-3 px-2 text-primary text-decoration-none d-flex align-items-center fw-bold"
+          :class="layoutStore.sidebarCollapsed ? '' : 'ms-2'"
+          :to="{ name: '/' }"
+        >
+          <img src="/logo.png" alt="" height="30" class="me-2" />
+          Enduro
+        </router-link>
+
+        <div class="flex-grow-1 d-none d-md-block">
+          <Breadcrumb />
+        </div>
+
+        <a
+          v-if="instLogo && instURL"
+          :href="instURL"
+          target="_blank"
+          rel="external"
+        >
+          <img
+            id="institution-logo"
+            :src="instLogo"
+            :alt="instName"
+            height="30"
+            class="d-none d-sm-block mx-3"
+          />
+        </a>
+        <img
+          id="institution-logo"
+          v-else-if="instLogo"
+          :src="instLogo"
+          :alt="instName"
+          height="30"
+          class="d-none d-sm-block mx-3"
         />
-      </button>
 
-      <!-- Collapse/expand sidebar button, visible in md or higher. -->
-      <button
-        type="button"
-        class="btn btn-link text-decoration-none p-3 d-none d-md-block"
-        :class="layoutStore.sidebarCollapsed ? 'sidebar-collapsed' : ''"
-        :aria-label="
-          (layoutStore.sidebarCollapsed ? 'Expand' : 'Collapse') + ' navigation'
-        "
-        @click="layoutStore.toggleSidebar()"
-      >
-        <IconMenu
-          class="text-dark mx-1"
-          style="font-size: 1.5em"
-          aria-hidden="true"
-        />
-      </button>
-
-      <router-link
-        class="navbar-brand h1 mb-0 me-auto p-3 px-2 text-primary text-decoration-none d-flex align-items-center fw-bold"
-        :class="layoutStore.sidebarCollapsed ? '' : 'ms-2'"
-        :to="{ name: '/' }"
-      >
-        <img src="/logo.png" alt="" height="30" class="me-2" />
-        Enduro
-      </router-link>
-
-      <div class="flex-grow-1 d-none d-md-block">
-        <Breadcrumb />
+        <button
+          type="button"
+          class="btn btn-link text-decoration-none p-3"
+          aria-label="About Enduro"
+        >
+          <IconInfo
+            class="text-primary mx-1"
+            style="font-size: 1.5em"
+            aria-hidden="true"
+            @click="showAbout"
+          />
+        </button>
       </div>
-
-      <button
-        type="button"
-        class="btn btn-link text-decoration-none p-3"
-        aria-label="About Enduro"
-      >
-        <IconInfo
-          class="text-primary mx-1"
-          style="font-size: 1.5em"
-          aria-hidden="true"
-          @click="showAbout"
-        />
-      </button>
     </nav>
   </header>
 </template>
