1256fc7f80347d6506ac35138ee57e8768ee7497
José Raddaoui Marín <raddaouimarin@gmail.com>
2025-11-27 18:08:14 +0100

    Start SIP workflows from Batch workflow
    Create SIPs, start their workflows and wait for completion.

--- internal/workflow/batch.go
+++ internal/workflow/batch.go
@@ -4,11 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"time"
 
+	"github.com/google/uuid"
+	temporalapi_enums "go.temporal.io/api/enums/v1"
 	temporalsdk_client "go.temporal.io/sdk/client"
 	temporalsdk_workflow "go.temporal.io/sdk/workflow"
 
 	"github.com/artefactual-sdps/enduro/internal/config"
+	"github.com/artefactual-sdps/enduro/internal/datatypes"
 	"github.com/artefactual-sdps/enduro/internal/enums"
 	"github.com/artefactual-sdps/enduro/internal/ingest"
 )
@@ -38,8 +42,8 @@ func (w *BatchWorkflow) Execute(ctx temporalsdk_workflow.Context, req *ingest.Ba
 	state := newBatchWorkflowState(ctx, req)
 	state.logger.Info(
 		"Starting batch workflow",
-		"batch_uuid", req.Batch.UUID.String(),
-		"batch_identifier", req.Batch.Identifier,
+		"uuid", req.Batch.UUID.String(),
+		"identifier", req.Batch.Identifier,
 		"source_id", req.SIPSourceID.String(),
 		"keys", req.Keys,
 	)
@@ -54,12 +58,37 @@ func (w *BatchWorkflow) Execute(ctx temporalsdk_workflow.Context, req *ingest.Ba
 		if err := w.updateBatch(ctx, state); err != nil {
 			e = errors.Join(e, err)
 		}
+
+		state.logger.Info(
+			"Batch workflow completed",
+			"uuid", state.batch.UUID.String(),
+			"identifier", state.batch.Identifier,
+			"status", state.batch.Status,
+		)
 	}()
 
 	// Update batch status to "processing" and add start date.
 	state.batch.Status = enums.BatchStatusProcessing
 	state.batch.StartedAt = temporalsdk_workflow.Now(ctx)
 	if err := w.updateBatch(ctx, state); err != nil {
+		return err
+	}
+
+	// Create SIPs and start processing workflows.
+	for i, key := range req.Keys {
+		if err := w.startSIPWorkflow(ctx, state, i, key, req.SIPSourceID); err != nil {
+			return err
+		}
+	}
+
+	// TODO:
+	// - Consider errors in child workflows and handle retries or failures.
+	// - Wait for all SIPs validation and send signals to continue/stop processing.
+	// - Check for all SIPs final status and update batch status.
+	// - Handle retention periods here or in child workflows.
+
+	// Wait for all SIP workflows to complete.
+	if err := w.waitForWorkflowsCompletion(ctx, state); err != nil {
 		return err
 	}
 
@@ -81,6 +110,95 @@ func (w *BatchWorkflow) updateBatch(ctx temporalsdk_workflow.Context, state *bat
 	).Get(activityOpts, nil)
 	if err != nil {
 		return fmt.Errorf("update batch: %v", err)
+	}
+
+	return nil
+}
+
+func (w *BatchWorkflow) startSIPWorkflow(
+	ctx temporalsdk_workflow.Context,
+	state *batchWorkflowState,
+	index int,
+	key string,
+	sourceID uuid.UUID,
+) error {
+	// Generate SIP UUID using SideEffect to ensure determinism.
+	var sipUUID uuid.UUID
+	genUUID := temporalsdk_workflow.SideEffect(ctx, func(ctx temporalsdk_workflow.Context) any {
+		return uuid.Must(uuid.NewRandomFromReader(w.rng))
+	})
+	if err := genUUID.Get(&sipUUID); err != nil {
+		return fmt.Errorf("generate SIP UUID: %v", err)
+	}
+
+	// Create SIP.
+	sip := datatypes.SIP{
+		UUID:     sipUUID,
+		Name:     key,
+		Status:   enums.SIPStatusQueued,
+		Batch:    &state.batch,
+		Uploader: state.batch.Uploader,
+	}
+	activityOpts := withLocalActivityOpts(ctx)
+	err := temporalsdk_workflow.ExecuteLocalActivity(
+		activityOpts,
+		createSIPLocalActivity,
+		w.ingestsvc,
+		&createSIPLocalActivityParams{SIP: sip},
+	).Get(activityOpts, nil)
+	if err != nil {
+		return fmt.Errorf("create SIP: %v", err)
+	}
+
+	// Start processing workflow for the SIP, keeping track of the workflow future and execution.
+	var we temporalsdk_workflow.Execution
+	processingCtx := temporalsdk_workflow.WithChildOptions(ctx, temporalsdk_workflow.ChildWorkflowOptions{
+		Namespace:         w.cfg.Temporal.Namespace,
+		TaskQueue:         w.cfg.Temporal.TaskQueue,
+		WorkflowID:        fmt.Sprintf("%s-%s", ingest.ProcessingWorkflowName, sipUUID.String()),
+		ParentClosePolicy: temporalapi_enums.PARENT_CLOSE_POLICY_TERMINATE,
+	})
+	wf := temporalsdk_workflow.ExecuteChildWorkflow(
+		processingCtx,
+		ingest.ProcessingWorkflowName,
+		&ingest.ProcessingWorkflowRequest{
+			SIPUUID:         sipUUID,
+			SIPName:         key,
+			Key:             key,
+			SIPSourceID:     sourceID,
+			Type:            enums.WorkflowTypeCreateAip,
+			RetentionPeriod: -1 * time.Second,
+			// Don't pass BatchUUID yet to avoid waiting for validation.
+			// BatchUUID:       state.batch.UUID,
+		},
+	)
+	err = wf.GetChildWorkflowExecution().Get(processingCtx, &we)
+	if err != nil {
+		return fmt.Errorf("processing workflow: %v", err)
+	}
+
+	// Store SIP details in the batch workflow state.
+	state.addSIPDetails(index, sip, wf, we)
+
+	return nil
+}
+
+func (w *BatchWorkflow) waitForWorkflowsCompletion(ctx temporalsdk_workflow.Context, state *batchWorkflowState) error {
+	selector := temporalsdk_workflow.NewSelector(ctx)
+	for _, sd := range state.sipDetails {
+		selector.AddFuture(sd.workflowFuture, func(f temporalsdk_workflow.Future) {
+			// Ignore error and result, we just want to know when it's done.
+			_ = f.Get(ctx, nil)
+		})
+	}
+
+	// Wait for all SIP workflows to complete or context cancellation.
+	// Block once per SIP/future.
+	for range state.sipDetails {
+		selector.Select(ctx)
+		if err := ctx.Err(); err != nil {
+			return fmt.Errorf("waiting for workflows: %v", err)
+		}
 	}
 
 	return nil
--- internal/workflow/batch_test.go
+++ internal/workflow/batch_test.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	temporalsdk_testsuite "go.temporal.io/sdk/testsuite"
+	temporalsdk_workflow "go.temporal.io/sdk/workflow"
 	"go.uber.org/mock/gomock"
 
 	"github.com/artefactual-sdps/enduro/internal/config"
@@ -18,9 +20,17 @@ import (
 	ingest_fake "github.com/artefactual-sdps/enduro/internal/ingest/fake"
 )
 
+const (
+	batchIdentifier = "test-batch"
+	batchSIP1Key    = "sip1.zip"
+	batchSIP2Key    = "sip2.zip"
+)
+
 var (
-	batchUUID = uuid.MustParse("f47ac10b-58cc-4372-a567-0e02b2c3d479")
-	sourceID  = uuid.MustParse("6ba7b814-9dad-41d1-80b4-00c04fd430c8")
+	sourceID      = uuid.MustParse("6ba7b814-9dad-41d1-80b4-00c04fd430c8")
+	batchUUID     = uuid.MustParse("f47ac10b-58cc-4372-a567-0e02b2c3d479")
+	batchSIP1UUID = uuid.MustParse("52fdfc07-2182-454f-963f-5f0f9a621d72")
+	batchSIP2UUID = uuid.MustParse("9566c74d-1003-4c4d-bbbb-0407d1e2c649")
 )
 
 type BatchWorkflowTestSuite struct {
@@ -32,6 +42,10 @@ type BatchWorkflowTestSuite struct {
 	workflow *BatchWorkflow
 }
 
+func processingChildWorkflow(ctx temporalsdk_workflow.Context, req *ingest.ProcessingWorkflowRequest) error {
+	return nil
+}
+
 func (s *BatchWorkflowTestSuite) SetupWorkflowTest(cfg config.Configuration) {
 	s.env = s.NewTestWorkflowEnvironment()
 	s.env.SetStartTime(startTime)
@@ -39,6 +53,11 @@ func (s *BatchWorkflowTestSuite) SetupWorkflowTest(cfg config.Configuration) {
 	ctrl := gomock.NewController(s.T())
 	ingestsvc := ingest_fake.NewMockService(ctrl)
 	rng := rand.New(rand.NewSource(1)) // #nosec: G404
+
+	s.env.RegisterWorkflowWithOptions(
+		processingChildWorkflow,
+		temporalsdk_workflow.RegisterOptions{Name: ingest.ProcessingWorkflowName},
+	)
 
 	s.workflow = NewBatchWorkflow(cfg, rng, ingestsvc, nil)
 }
@@ -68,12 +87,16 @@ func TestBatchWorkflow(t *testing.T) {
 
 // TestBatch tests:
 // - Batch status updates (Processing -> Ingested).
+// - SIP creation for each key.
+// - Child processing workflows started for each SIP.
+// - Waiting for all child workflows to complete.
 func (s *BatchWorkflowTestSuite) TestBatch() {
 	s.SetupWorkflowTest(config.Configuration{})
 
+	// Mock initial batch status update.
 	s.env.OnActivity(
 		updateBatchLocalActivity,
-		mock.AnythingOfType("*context.valueCtx"),
+		ctx,
 		s.workflow.ingestsvc,
 		&updateBatchLocalActivityParams{
 			UUID:      batchUUID,
@@ -82,9 +105,84 @@ func (s *BatchWorkflowTestSuite) TestBatch() {
 		},
 	).Return(&updateBatchLocalActivityResult{}, nil)
 
+	// Mock SIP creation for the first SIP.
+	s.env.OnActivity(
+		createSIPLocalActivity,
+		ctx,
+		s.workflow.ingestsvc,
+		&createSIPLocalActivityParams{
+			SIP: datatypes.SIP{
+				UUID:   batchSIP1UUID,
+				Name:   batchSIP1Key,
+				Status: enums.SIPStatusQueued,
+				Batch: &datatypes.Batch{
+					UUID:       batchUUID,
+					Identifier: batchIdentifier,
+					Status:     enums.BatchStatusProcessing,
+					SIPSCount:  2,
+					CreatedAt:  startTime,
+					StartedAt:  startTime,
+				},
+			},
+		},
+	).Return(1, nil)
+
+	// Mock the processing workflow call for the first SIP.
+	s.env.OnWorkflow(
+		processingChildWorkflow,
+		internalCtx,
+		&ingest.ProcessingWorkflowRequest{
+			SIPUUID:         batchSIP1UUID,
+			SIPName:         batchSIP1Key,
+			Key:             batchSIP1Key,
+			SIPSourceID:     sourceID,
+			Type:            enums.WorkflowTypeCreateAip,
+			RetentionPeriod: -1 * time.Second,
+			// BatchUUID:       batchUUID,
+		},
+	).Return(nil)
+
+	// Mock SIP creation for the second SIP.
+	s.env.OnActivity(
+		createSIPLocalActivity,
+		ctx,
+		s.workflow.ingestsvc,
+		&createSIPLocalActivityParams{
+			SIP: datatypes.SIP{
+				UUID:   batchSIP2UUID,
+				Name:   batchSIP2Key,
+				Status: enums.SIPStatusQueued,
+				Batch: &datatypes.Batch{
+					UUID:       batchUUID,
+					Identifier: batchIdentifier,
+					Status:     enums.BatchStatusProcessing,
+					SIPSCount:  2,
+					CreatedAt:  startTime,
+					StartedAt:  startTime,
+				},
+			},
+		},
+	).Return(2, nil)
+
+	// Mock the processing workflow call for the second SIP.
+	s.env.OnWorkflow(
+		processingChildWorkflow,
+		internalCtx,
+		&ingest.ProcessingWorkflowRequest{
+			SIPUUID:         batchSIP2UUID,
+			SIPName:         batchSIP2Key,
+			Key:             batchSIP2Key,
+			SIPSourceID:     sourceID,
+			Type:            enums.WorkflowTypeCreateAip,
+			RetentionPeriod: -1 * time.Second,
+			// BatchUUID:       batchUUID,
+		},
+	).Return(nil)
+
+	// Mock final batch status update.
 	s.env.OnActivity(
 		updateBatchLocalActivity,
-		mock.AnythingOfType("*context.valueCtx"),
+		ctx,
 		s.workflow.ingestsvc,
 		&updateBatchLocalActivityParams{
 			UUID:        batchUUID,
@@ -97,13 +195,13 @@ func (s *BatchWorkflowTestSuite) TestBatch() {
 	s.ExecuteAndValidateWorkflow(&ingest.BatchWorkflowRequest{
 		Batch: datatypes.Batch{
 			UUID:       batchUUID,
-			Identifier: "test-batch",
+			Identifier: batchIdentifier,
 			Status:     enums.BatchStatusQueued,
 			CreatedAt:  startTime,
 			SIPSCount:  2,
 		},
 		SIPSourceID: sourceID,
-		Keys:        []string{"sip1.zip", "sip2.zip"},
+		Keys:        []string{batchSIP1Key, batchSIP2Key},
 	}, false)
 }
 
@@ -116,7 +214,7 @@ func (s *BatchWorkflowTestSuite) TestBatchError() {
 	// handle retries (3 + 3) and dynamic completion times.
 	s.env.OnActivity(
 		updateBatchLocalActivity,
-		mock.AnythingOfType("*context.valueCtx"),
+		ctx,
 		s.workflow.ingestsvc,
 		mock.AnythingOfType("*workflow.updateBatchLocalActivityParams"),
 	).Return(nil, fmt.Errorf("update error")).Times(6)
@@ -124,12 +222,12 @@ func (s *BatchWorkflowTestSuite) TestBatchError() {
 	s.ExecuteAndValidateWorkflow(&ingest.BatchWorkflowRequest{
 		Batch: datatypes.Batch{
 			UUID:       batchUUID,
-			Identifier: "test-batch",
+			Identifier: batchIdentifier,
 			Status:     enums.BatchStatusQueued,
 			CreatedAt:  startTime,
 			SIPSCount:  2,
 		},
 		SIPSourceID: sourceID,
-		Keys:        []string{"sip1.zip", "sip2.zip"},
+		Keys:        []string{batchSIP1Key, batchSIP2Key},
 	}, true)
 }
