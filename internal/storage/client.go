5ad368c6c32af46d1cfce3da93ae4a9fbfea503b
David Juhasz <djjuhasz@gmail.com>
2025-12-01 17:49:10 -0800

    Re-order storage Client methods
    Order storage client methods by entity and function.

--- internal/storage/client.go
+++ internal/storage/client.go
@@ -8,28 +8,36 @@ import (
 )
 
 type Client interface {
-	MonitorRequest(context.Context, *goastorage.MonitorRequestPayload) (*goastorage.MonitorRequestResult, error)
-	Monitor(context.Context, *goastorage.MonitorPayload) (goastorage.MonitorClientStream, error)
+	CreateAip(context.Context, *goastorage.CreateAipPayload) (*goastorage.AIP, error)
 	ListAips(context.Context, *goastorage.ListAipsPayload) (*goastorage.AIPs, error)
+	ShowAip(context.Context, *goastorage.ShowAipPayload) (*goastorage.AIP, error)
+
 	SubmitAip(context.Context, *goastorage.SubmitAipPayload) (*goastorage.SubmitAIPResult, error)
 	SubmitAipComplete(context.Context, *goastorage.SubmitAipCompletePayload) error
-	CreateAip(context.Context, *goastorage.CreateAipPayload) (*goastorage.AIP, error)
+
 	DownloadAipRequest(
 		context.Context,
 		*goastorage.DownloadAipRequestPayload,
 	) (*goastorage.DownloadAipRequestResult, error)
 	DownloadAip(context.Context, *goastorage.DownloadAipPayload) (*goastorage.DownloadAipResult, io.ReadCloser, error)
-	ListLocations(context.Context, *goastorage.ListLocationsPayload) (goastorage.LocationCollection, error)
-	CreateLocation(context.Context, *goastorage.CreateLocationPayload) (*goastorage.CreateLocationResult, error)
+
 	MoveAip(context.Context, *goastorage.MoveAipPayload) error
 	MoveAipStatus(context.Context, *goastorage.MoveAipStatusPayload) (*goastorage.MoveStatusResult, error)
+
 	RejectAip(context.Context, *goastorage.RejectAipPayload) error
-	ShowAip(context.Context, *goastorage.ShowAipPayload) (*goastorage.AIP, error)
-	ListAipWorkflows(context.Context, *goastorage.ListAipWorkflowsPayload) (*goastorage.AIPWorkflows, error)
+
 	RequestAipDeletion(context.Context, *goastorage.RequestAipDeletionPayload) error
 	ReviewAipDeletion(context.Context, *goastorage.ReviewAipDeletionPayload) error
+
+	CreateLocation(context.Context, *goastorage.CreateLocationPayload) (*goastorage.CreateLocationResult, error)
+	ListLocations(context.Context, *goastorage.ListLocationsPayload) (goastorage.LocationCollection, error)
 	ShowLocation(context.Context, *goastorage.ShowLocationPayload) (*goastorage.Location, error)
 	ListLocationAips(context.Context, *goastorage.ListLocationAipsPayload) (goastorage.AIPCollection, error)
+
+	ListAipWorkflows(context.Context, *goastorage.ListAipWorkflowsPayload) (*goastorage.AIPWorkflows, error)
+
+	MonitorRequest(context.Context, *goastorage.MonitorRequestPayload) (*goastorage.MonitorRequestResult, error)
+	Monitor(context.Context, *goastorage.MonitorPayload) (goastorage.MonitorClientStream, error)
 }
 
 var _ Client = (*goastorage.Client)(nil)
