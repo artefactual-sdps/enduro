b0df0287dcf39a45ed09c9251e9913a094b11904
José Raddaoui Marín <raddaouimarin@gmail.com>
2026-03-09 21:36:26 +0100

    Use go.artefactual.dev/tools/clientauth
    [skip-codecov]

--- internal/ingest/config.go
+++ internal/ingest/config.go
@@ -2,18 +2,11 @@ package ingest
 
 import (
 	"errors"
+	"fmt"
 	"strings"
-	"time"
 
 	"github.com/google/uuid"
-)
-
-const (
-	DefaultStorageOIDCTokenExpiryLeeway       = 30 * time.Second
-	DefaultStorageOIDCRetryMaxAttempts        = 3
-	DefaultStorageOIDCRetryInitialInterval    = 500 * time.Millisecond
-	DefaultStorageOIDCRetryMaxInterval        = 2 * time.Second
-	DefaultStorageOIDCRetryBackoffCoefficient = 2.0
+	"go.artefactual.dev/tools/clientauth"
 )
 
 type Config struct {
@@ -27,18 +20,8 @@ type StorageConfig struct {
 }
 
 type StorageOIDCConfig struct {
-	Enabled                 bool
-	ProviderURL             string
-	TokenURL                string
-	ClientID                string
-	ClientSecret            string
-	Scopes                  []string
-	Audience                string
-	TokenExpiryLeeway       time.Duration
-	RetryMaxAttempts        int
-	RetryInitialInterval    time.Duration
-	RetryMaxInterval        time.Duration
-	RetryBackoffCoefficient float64
+	Enabled                                  bool
+	clientauth.OIDCAccessTokenProviderConfig `mapstructure:",squash"`
 }
 
 func (c Config) Validate() error {
@@ -67,27 +50,9 @@ func (c StorageOIDCConfig) Validate() error {
 		return nil
 	}
 
-	var errs []error
-	if c.ProviderURL == "" && c.TokenURL == "" {
-		errs = append(errs, errors.New("missing OIDC providerURL or tokenURL with storage OIDC auth. enabled"))
-	}
-	if c.ClientID == "" || c.ClientSecret == "" {
-		errs = append(errs, errors.New("missing OIDC client credentials with storage OIDC auth. enabled"))
-	}
-	if c.RetryMaxAttempts < 0 {
-		errs = append(errs, errors.New("invalid storage OIDC retry max attempts, value must be >= 0"))
-	}
-	if c.RetryInitialInterval < 0 || c.RetryMaxInterval < 0 || c.TokenExpiryLeeway < 0 {
-		errs = append(errs, errors.New("invalid storage OIDC duration configuration, values must be >= 0"))
-	}
-	if c.RetryInitialInterval > 0 && c.RetryMaxInterval > 0 && c.RetryMaxInterval < c.RetryInitialInterval {
-		errs = append(errs, errors.New(
-			"invalid storage OIDC retry interval configuration, max interval must be >= initial interval",
-		))
-	}
-	if c.RetryBackoffCoefficient != 0 && c.RetryBackoffCoefficient < 1 {
-		errs = append(errs, errors.New("invalid storage OIDC retry backoff coefficient, value must be >= 1"))
+	if err := c.OIDCAccessTokenProviderConfig.Validate(); err != nil {
+		return fmt.Errorf("storage OIDC:\n%v", err)
 	}
 
-	return errors.Join(errs...)
+	return nil
 }
